bce1000aec94db7bd1697f62774666448d063de7
ianhancock1970 <ian@miromedia.co.uk>
2024-08-02 17:14:40 +0100

    removed duplication

--- phone-field-formatter-e164.js
+++ phone-field-formatter-e164.js
@@ -14,22 +14,5 @@ function initializeLibphonenumber() {
     }
   });
 }
-
-import { parsePhoneNumberFromString } from 'libphonenumber-js';
-
-// Function to initialize libphonenumber-js
-function initializeLibphonenumber() {
-  document.addEventListener('input', (event) => {
-    const element = event.target;
-    if (element.tagName === 'INPUT' && /phone/i.test(element.name)) {
-      const phoneNumber = element.value.replace(/\s/g, '');
-      const parsedPhoneNumber = parsePhoneNumberFromString(phoneNumber, 'GB');
-      if (parsedPhoneNumber) {
-        element.value = parsedPhoneNumber.formatInternational().replace(/\s/g, '');
-      }
-    }
-  });
-}
-
 // Initialize libphonenumber-js
 initializeLibphonenumber();
